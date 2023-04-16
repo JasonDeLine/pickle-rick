c2409a35f98de224990322507730ed8b106c6493
Jason DeLine <jcdeline1982@gmail.com>
2023-04-16 10:28:40 -0500

    adding questions array

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utilities/generateMarkdown');
+
+// array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Please enter the project name:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please enter a brief description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please enter any installation instructions for your project:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage instructions for your project:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'Unlicense'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide instructions for running tests on your project:',
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
